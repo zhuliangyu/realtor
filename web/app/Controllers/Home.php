e21eae4a5d01009b694e4bf9fafc4285319d7b72
Ricky Zhu <ricky@enbut.com>
2020-06-13 03:51:18 -0700

    change home page

--- web/app/Controllers/Home.php
+++ web/app/Controllers/Home.php
@@ -5,11 +5,7 @@ class Home extends BaseController
 
 	public function index()
 	{
-        $sql = "SELECT * FROM SELLER";
-        $db = \Config\Database::connect();
-        $test = $db->simpleQuery($sql);
-        var_dump($test);
-        return view('welcome_message');
+	    return view('welcome_message');
 	}
 
 	//--------------------------------------------------------------------

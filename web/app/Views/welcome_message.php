304873da91e5dc3257dcf5dd3b9d707c62c71ebc
Yang Xia <yxia@bccrc.ca>
2020-06-16 11:08:47 -0700

    changes to the landing apge

--- web/app/Views/welcome_message.php
+++ web/app/Views/welcome_message.php
@@ -6,29 +6,47 @@ img {
 }
 
 .headline {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 36px; 
+    font-family: Trebuchet MS, Helvetica, sans-serif;
+    font-size: 3.5vw;
+    padding: 2px 2px;
     text-align: center;
+    background-color: white;
+    color: lightblue;
+    opacity: 1;
+    margin-top: 30px;
 }
 
 .button {
   background-color: white;
   border: none;
-  color: white;
-  padding: 15px 32px;
-  font-family: Arial, Helvetica, sans-serif;
+  border-radius:10px;
+  opacity: 0.8;
+  color: lightblue;
+  padding: 8px 8px;
+  font-family: Trebuchet MS, Helvetica, sans-serif;
   text-align: center;
-  text-decoration: none;
   display: inline-block;
-  font-size: 24px;
-  position: fixed;
-  top: 90%;
-  left: 40%;
+  font-size: 2.2vw;
+  position: center;
+  margin-left: 39vw;
+  margin-top: 9.6vw;
 }
+
+.body {
+  /* background-image: url("https://i.ibb.co/9GsYgtV/building-metal-house-architecture-101808.jpg"); */
+  background-image: url("https://i.ibb.co/7gJs5xn/assorted-color-wall-paint-house-photo-1370704.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+
 </style>
 
 <h1 class = "headline">Welcome to RealtorInsiderVIP.ca</h1>
-<div>
+<body class="body">
+<!-- <div>
     <img src="https://picsum.photos/500/600" alt="">
-</div>
+</div> -->
+</body>
+
 <button class="button"><a href="<?php echo site_url('houses')?>">View Current Listing</a></button>

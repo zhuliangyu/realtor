367ea39717b44423e7b9a4ef8a49a1bd661e685f
Ricky Zhu <ricky@enbut.com>
2020-06-16 19:20:46 -0700

    NOT SHOW ANY HOUSE WHICH HAS NULL ADDRESS_ID

--- web/app/Controllers/Houses.php
+++ web/app/Controllers/Houses.php
@@ -11,7 +11,7 @@ class Houses extends BaseController
     //http://realtor:8888/houses
     public function index()
     {
-        $sql_showAll = "SELECT HOUSE_ID, ADDRESS_ID, LISTING_DATE, LISTING_PRICE, SELLER_ID, REALTOR_ID, FLOOR_SPACE, PROPERTY_TYPE_ID FROM `HOUSE`";
+        $sql_showAll = "SELECT HOUSE_ID, ADDRESS_ID, LISTING_DATE, LISTING_PRICE, SELLER_ID, REALTOR_ID, FLOOR_SPACE, PROPERTY_TYPE_ID FROM `HOUSE` WHERE ADDRESS_ID IS NOT NULL";
         $sql_count = "SELECT COUNT(*) AS COUNT FROM HOUSE;";
         $houseList_query = $this->db->query($sql_showAll);
         $data['houseList'] = $houseList_query->getResult();
